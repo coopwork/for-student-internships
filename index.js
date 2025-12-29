4056597ac3794fe2f02de6f7808bae5a4aefc876
Prestoll <mukhamed.sattarov@gmail.com>
2025-12-29 20:02:51 +0500

    создали два элемента img article

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { createElement } from "./assets/js/create-element.js";
 import { fetchProducts } from "./assets/js/fetch-products.js";
 
 const shoppingCartElements = {
@@ -27,9 +28,22 @@ async function getProducts() {
 
   console.log('Список продуктов', products);
   console.table(products);
+
+  products.forEach( item => {
+    const img = createElement({ tag: "img", className: "product__cover",
+
+    })
+    const article = createElement({ tag: "article", className: "product__card", attrs: {
+      "data-in-cart": "false"
+    }
+
+    })
+  })
 }
 
 getProducts();
+
+
 
 
 
